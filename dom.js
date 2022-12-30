6920e483e6749e9da337d83e09b5af3c4374387f
Vpanwar-code <vivekpanwar183@gmail.com>
2022-12-30 14:55:36 +0530

    update2 dom.js

--- dom.js
+++ dom.js
@@ -4,19 +4,37 @@ console.log(manipulate.textContent);
 var mainheader=document.getElementById('main-header');
 mainheader.style.borderBottom='solid 3px #000';
 
-var title=document.getElementsByClassName('list-group-item');
-console.log(title[1].textContent);
-title[1].style.fontWeight='bold';
-title[1].style.color='green';
+//var title=document.getElementsByClassName('list-group-item');
+//console.log(title[1].textContent);
+//title[1].style.fontWeight='bold';
+//title[1].style.color='green';
 
-title[2].style.backgroundColor='green';
+// title[2].style.backgroundColor='green';
 
-for(var i=0; i<title.length; i++){
-    title[i].style.fontWeight='bold';
+// for(var i=0; i<title.length; i++){
+//    title[i].style.fontWeight='bold';
+// }
+
+// var items= document.getElementsByTagName('li');
+// items[4].style.color='blue';
+
+// var items1=document.getElementByClassName('new-item');
+// items1.style.fontWeight='bold';
+
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+
+//var secondChild=document.querySelector('.list-group-item:nth-child(2) ');
+//secondChild.style.backgroundColor='green';
+
+//var thirdChild=document.querySelector('.list-group-item:nth-child(3) ');
+//thirdChild.style.visibility='hidden';
+
+var items=document.querySelectorAll('.list-group-item');
+items[1].style.color='green';
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor='#f4f4f4';
 }
-
-var items= document.getElementsByTagName('li');
-items[4].style.color='blue';
-
-var items1=document.getElementByClassName('new-item');
-items1.style.fontWeight='bold';
