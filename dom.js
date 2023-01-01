0f76fc4b92f9dccf30af84fdf1597d7353b6f736
Vpanwar-code <vivekpanwar183@gmail.com>
2023-01-01 21:02:33 +0530

    update3 dom.js

--- dom.js
+++ dom.js
@@ -30,11 +30,61 @@ header.style.borderBottom='solid 4px #ccc';
 //var thirdChild=document.querySelector('.list-group-item:nth-child(3) ');
 //thirdChild.style.visibility='hidden';
 
-var items=document.querySelectorAll('.list-group-item');
-items[1].style.color='green';
+// var items=document.querySelectorAll('.list-group-item');
+// items[1].style.color='green';
 
-var odd=document.querySelectorAll('li:nth-child(odd)');
+// var odd=document.querySelectorAll('li:nth-child(odd)');
 
-for(var i=0; i<odd.length; i++){
-    odd[i].style.backgroundColor='#f4f4f4';
-}
+// for(var i=0; i<odd.length; i++){
+//     odd[i].style.backgroundColor='#f4f4f4';
+// }
+
+var itemList = document.querySelector('#items');
+//itemList.parentNode.style.backgroundColor='#f4f4f4';
+
+itemList.parentElement.style.backgroundColor='#f4f4f4';
+
+itemList.children[1].style.backgroundColor='#f4f4f4';
+
+console.log(itemList.firstChild);
+
+itemList.firstElementChild.style.backgroundColor='yellow';
+
+itemList.lastElementChild.textContent='hello 4';
+
+console.log(itemList.nextElementSibling);
+
+console.log(itemList.previousElementSibling);
+
+itemList.previousElementSibling.style.color='blue'
+
+
+//creating element using javascript
+
+var newDiv =document.createElement('div');
+
+newDiv.className='hello';
+newDiv.id='hello 1';
+
+newDiv.setAttribute('title','Hello div');
+
+var textOfDiv=document.createTextNode('welcome to new div');
+
+newDiv.appendChild(textOfDiv);
+
+var container=document.querySelector('header .container');
+
+var h1=document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1);
+
+
+
+
+
+
+
+
+
+
+
