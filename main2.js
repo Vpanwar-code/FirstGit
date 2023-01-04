b627eb66187e6bd8171039795c2a8c25a0528912
Vpanwar-code <vivekpanwar183@gmail.com>
2023-01-04 22:07:35 +0530

    updatemain2

--- main2.js
+++ main2.js
@@ -9,4 +9,10 @@ function saveToLocalStorage(event){
         email
     }
     localStorage.setItem('userdetails', JSON.stringify(obj));
+    showUserOnScreen(obj);
+
+    function showUserOnScreen(obj){
+        const parent=document.getElementById('users');
+        parent.innerHTML=parent.innerHTML+`<li>${obj.name}-${obj.email}</li>`;
+    }
 }
