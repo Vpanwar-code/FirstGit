781730a1450680b22f1ee2a7de7f5565a12d0598
Vpanwar-code <vivekpanwar183@gmail.com>
2023-03-26 13:10:30 +0530

    main update

--- main2.js
+++ main2.js
@@ -2,14 +2,27 @@ function saveToLocalStorage(event){
     event.preventDefault();
     const name=event.target.username.value;
     const email=event.target.useremail.value;
+    const number=event.target.usernumber.value;
     localStorage.setItem('name',name);
     localStorage.setItem('emailid',email);
+    localStorage.setItem('phone_number',number);
+
     const obj={
         name,
-        email
+        email,
+        number
     }
-    localStorage.setItem('userdetails', JSON.stringify(obj));
-    showUserOnScreen(obj);
+
+    axios.post("https://crudcrud.com/api/b666e5a8d45e4db6b26327875696f2c1")
+    .then((response)=>{
+        showUserOnScreen(response);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+   // localStorage.setItem('userdetails', JSON.stringify(obj));
+
+    //showUserOnScreen(obj);
 
     function showUserOnScreen(obj){
         const parentele=document.getElementById('users');
